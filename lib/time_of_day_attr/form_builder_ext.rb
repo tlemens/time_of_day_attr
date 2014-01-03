8bc0944807b2bcc91ed9581e83c3075a43d4a04c
Clemens <clemens@maya.(none)>
2014-01-03 19:07:07 +0100

    Remove space

--- lib/time_of_day_attr/form_builder_ext.rb
+++ lib/time_of_day_attr/form_builder_ext.rb
@@ -2,7 +2,7 @@ module TimeOfDayAttr
   module FormBuilderExt
     extend ActiveSupport::Concern
 
-    def  time_of_day_field(method, options = {})
+    def time_of_day_field(method, options = {})
       unless options[:value]
         value = object.send(method)
         options[:value] = TimeOfDayAttr.localize(value, options.extract!(:format, :omit_minutes_at_full_hour))
